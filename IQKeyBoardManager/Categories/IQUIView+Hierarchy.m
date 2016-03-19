af3e2e884783ad12e7a7bfcbdafa073b14da3667
Dima Rozumeenko <d.rozumeenko@synergetica.net>
2016-03-19 11:03:25 +0200

    UISearchBar support

--- IQKeyBoardManager/Categories/IQUIView+Hierarchy.m
+++ IQKeyBoardManager/Categories/IQUIView+Hierarchy.m
@@ -142,7 +142,7 @@ Class UISearchBarTextFieldClass;        //UISearchBar
 -(BOOL)_IQcanBecomeFirstResponder
 {
     [self _setIsAskingCanBecomeFirstResponder:YES];
-    BOOL _IQcanBecomeFirstResponder = ([self canBecomeFirstResponder] && [self isUserInteractionEnabled] && ![self isHidden] && [self alpha]!=0.0 && ![self isAlertViewTextField]  && ![self isSearchBarTextField]);
+    BOOL _IQcanBecomeFirstResponder = ([self canBecomeFirstResponder] && [self isUserInteractionEnabled] && ![self isHidden] && [self alpha]!=0.0 && ![self isAlertViewTextField]);
     
     if (_IQcanBecomeFirstResponder == YES)
     {
